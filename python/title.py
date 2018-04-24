8a3627cb8cb4393797d95a3e32c35c4afcbf11d8
Wayne Lee <waynelee1000@gmail.com>
2018-04-24 10:24:01 -0400

    Fix display bugs
    fixed repeated days and repeated work titles

--- python/title.py
+++ python/title.py
@@ -15,19 +15,19 @@ title = div.find('h2')
 
 date = soup.find('span', {'class': 'date'})
 
-for div in soup.find_all('div'):
 
+
+for div in soup.find_all('div'):
     for title in div.find_all('h2'):
         print(title.text)
 
         for a in title.find_all('a', href=True):
             print("https://www.indeed.com"+a.get('href'))
 
-    for company in div.find_all('span',{'class': 'company'}):
-            print (company.text.lstrip())
+            for company in div.find_all('span',{'class': 'company'}):
+                print (company.text.lstrip())
 
-
-    for date in div.find_all('span', {'class': 'date'}):
-    		print(date.text)
+                for date in div.find_all('span', {'class': 'date'}):
+                    print(date.text)
 
 
