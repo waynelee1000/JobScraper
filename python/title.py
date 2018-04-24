4520da2a83e63aa4b7d5a33197801779d2de9309
Wayne Lee <waynelee1000@gmail.com>
2018-04-24 11:13:14 -0400

    Added comments and tried to use multiple urls
    Multiple urls is not working.
    Commments talks about gpa requirement.

--- python/title.py
+++ python/title.py
@@ -2,32 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-
-r = requests.get("https://www.indeed.com/jobs?q=IT+intern&l=19107&sort=date")
 sys.stdout = open("title.txt", "w")
 
-soup = BeautifulSoup(r.content)
+links = ['https://www.indeed.com/jobs?q=IT+intern&l=19107&sort=date',]
 
-div = soup.find('div')
+for url in links:
+    r = requests.get(url)
 
-title = div.find('h2')
+    soup = BeautifulSoup(r.content)
 
+    for div in soup.find_all('div'):
 
-date = soup.find('span', {'class': 'date'})
+        for title in div.find_all('h2'):
 
+            if 'Intern' in title.text or 'intern' in title.text:
+                print(title.text)
 
+                for a in title.find_all('a', href=True):
+                    print("https://www.indeed.com"+a.get('href'))
+                    """
+                    Inside td class =  snip 
+                    Check for GPA requirement
+                    If the TD class doesnt contain GPA allow
+                    if GPA exist create another if statement to check value
+                    """
 
-for div in soup.find_all('div'):
-    for title in div.find_all('h2'):
-        print(title.text)
+                    for company in div.find_all('span',{'class': 'company'}):
+                        print (company.text.lstrip())
 
-        for a in title.find_all('a', href=True):
-            print("https://www.indeed.com"+a.get('href'))
-
-            for company in div.find_all('span',{'class': 'company'}):
-                print (company.text.lstrip())
-
-                for date in div.find_all('span', {'class': 'date'}):
-                    print(date.text)
-
-
+                        for date in div.find_all('span', {'class': 'date'}):
+                            print(date.text)
